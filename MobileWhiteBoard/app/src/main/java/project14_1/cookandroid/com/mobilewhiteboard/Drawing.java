7b234eae2206290bb65dd27673654eeb233fb674
subin <sb_design@Naver.com>
2018-02-01 00:10:38 +0900

    * 화이트보드 1. 아이콘 추가 및 수정 2. 그림기능(그리기-색상 5가지 선택가능, 지우개, 전체삭제) 구현 3. 저장/불러오기 미구현

---
+++ MobileWhiteBoard/app/src/main/java/project14_1/cookandroid/com/mobilewhiteboard/Drawing.java
@@ -0,0 +1,80 @@
+package project14_1.cookandroid.com.mobilewhiteboard;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class Drawing extends View {
+    private Path drawPath;
+    public static Paint drawPaint, canvasPaint;
+    public static int paintColor = 0xFF000000;
+    private Canvas drawCanvas;
+    public static Bitmap canvasBitmap;
+
+
+    public Drawing(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged( w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap( w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawPath.lineTo(touchX, touchY);
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+    public void clear() {
+        canvasBitmap.eraseColor(Color.WHITE);
+        invalidate();
+    }
+}
--- MobileWhiteBoard/app/src/main/java/project14_1/cookandroid/com/mobilewhiteboard/WhiteboardActivity.java
+++ MobileWhiteBoard/app/src/main/java/project14_1/cookandroid/com/mobilewhiteboard/WhiteboardActivity.java
@@ -1,17 +1,90 @@
 package project14_1.cookandroid.com.mobilewhiteboard;
 
+
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 /**
  * Created by com on 2018-01-21.
  */
 public class WhiteboardActivity extends AppCompatActivity {
-
+    ImageButton btnClear;
+    ImageButton btnEraser;
+    ImageButton btnPen;
+    private String[] items = {"Black", "Red", "Blue", "Yellow", "Green"};
     protected void onCreate(Bundle savedIntanteState){
         super.onCreate(savedIntanteState);
         setContentView(R.layout.activity_whiteboard);
+
+        ImageButton btn_drawing = (ImageButton) findViewById(R.id.btnDrawing);
+
+        btn_drawing.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                View drawing = (View) findViewById(R.id.Drawing1);
+                View drawTool = (View) findViewById(R.id.DrawTool);
+                drawing.setVisibility(View.VISIBLE);
+                drawTool.setVisibility(View.VISIBLE);
+
+            }
+        });
+        btnClear = (ImageButton)findViewById(R.id.btnClear);
+        btnEraser = (ImageButton)findViewById(R.id.btnEraser);
+        btnPen = (ImageButton)findViewById(R.id.btnPen);
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Drawing.canvasBitmap.eraseColor(Color.WHITE);
+                view.invalidate();
+            }
+        });
+        btnEraser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Drawing.drawPaint.setColor(Color.WHITE);
+                Drawing.drawPaint.setStrokeWidth(200);
+                view.invalidate();
+            }
+        });
+        btnPen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder color = new AlertDialog.Builder(WhiteboardActivity.this);
+                color.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        switch (i) {
+                            case 0 :
+                                Drawing.drawPaint.setColor(Color.BLACK);
+                                break;
+                            case 1 :
+                                Drawing.drawPaint.setColor(Color.RED);
+                                break;
+                            case 2 :
+                                Drawing.drawPaint.setColor(Color.BLUE);
+                                break;
+                            case 3:
+                                Drawing.drawPaint.setColor(Color.YELLOW);
+                                break;
+                            default:
+                                Drawing.drawPaint.setColor(Color.GREEN);
+                        }
+                    }
+                });
+                color.create().show();
+                //Drawing.drawPaint.setColor(Color.BLACK);
+                Drawing.drawPaint.setStrokeWidth(20);
+            }
+        });
     }
 
     @Override
